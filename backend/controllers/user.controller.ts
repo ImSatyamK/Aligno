cf214c335b0d8f8c1b1cb33118e98747d6c1a739
Satyam Kumar <189542961+ImSatyamK@users.noreply.github.com>
2026-06-30 23:17:24 +0530

    Feature: Follow & Unfollow

--- backend/controllers/user.controller.ts
+++ backend/controllers/user.controller.ts
@@ -3,17 +3,50 @@ import { Request, Response } from "express";
 import User from '../models/user.model'
 
 
-export async function getUserProfile(req:Request, res: Response) {
+export async function getUserProfile(req: Request, res: Response) {
     const { username } = req.params
 
     try {
-       const user = await User.findOne({username}).select('-password')
-       if (!user){
-        return res.status(404).json({error: `No user found of username: ${username}`})
-       }
-       res.status(200).json(user)
+        const user = await User.findOne({ username }).select('-password')
+        if (!user) {
+            return res.status(404).json({ error: `No user found of username: ${username}` })
+        }
+        res.status(200).json(user)
     } catch (error) {
-        console.log({error: error})
-        return res.status(500).json({error: 'Internal server error'})
+        console.log({ error: error })
+        return res.status(500).json({ error: 'Internal server error' })
     }
+}
+
+export async function followUnfollowUser(req: Request, res: Response) {
+    try {
+        const { id } = req.params
+        if (!req.user) {
+            return res.status(400).json('Login first')
+        }
+        const currentUser = await User.findById(req.user._id).select('-password')
+        const userToModify = await User.findById(id).select('-password')
+
+        if (!userToModify || !currentUser) {
+            return res.status(404).json('User not found')
+        }
+        if (currentUser._id.toString() === userToModify._id.toString()) {
+            return res.status(400).json({ error: "You can't follow/unfollow yourself" })
+        }
+
+        const isFollowing = currentUser.following.includes(userToModify._id)
+        if (isFollowing) {
+            await User.findByIdAndUpdate(currentUser._id, {$pull: {following: userToModify._id}})
+            await User.findByIdAndUpdate(userToModify._id, {$pull: {followers: currentUser._id}})
+            res.status(200).json({ message: "User unfollowed successfully" })
+        } else {
+            await User.findByIdAndUpdate(currentUser._id, {$push: {following: userToModify._id}})
+            await User.findByIdAndUpdate(userToModify._id, {$push: {followers: currentUser._id}})
+            res.status(200).json({ message: "User followed successfully" })
+        }
+    } catch (error) {
+        console.log({ error: error })
+        return res.status(500).json({ error: 'Internal server error' })
+    }
+
 }
--- backend/routes/user.route.ts
+++ backend/routes/user.route.ts
@@ -1,11 +1,11 @@
 import express from 'express'
 
 import { protectRoute } from '../middleware/protectRoute'
-import { getUserProfile } from '../controllers/user.controller'
+import { getUserProfile, followUnfollowUser } from '../controllers/user.controller'
 
 export const userRouter = express.Router()
 
 userRouter.get('/profile/:username', protectRoute, getUserProfile)
 // userRouter.get('/suggested', protectRoute, getSuggestedUsers)
-// userRouter.post('/followUnfollow/:username', protectRoute, followUnfollowUser)
+userRouter.post('/followUnfollow/:id', protectRoute, followUnfollowUser)
 // userRouter.post('/update', protectRoute, updateUserProfile)
