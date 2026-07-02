60bc5f57cdab4431fb7df161d49039b728593389
Satyam Kumar <189542961+ImSatyamK@users.noreply.github.com>
2026-07-02 15:38:23 +0530

    Feature: Delete Post

--- backend/controllers/post.controller.ts
+++ backend/controllers/post.controller.ts
@@ -23,7 +23,7 @@ export async function createPost(req: Request, res: Response) {
         }
 
         if(img){
-            const uploaded = await claudinary.uploader.upload(img)
+            const uploaded = await claudinary.uploader.upload(img, {folder: 'post_images'})
             img = uploaded.secure_url
         }
 
@@ -33,10 +33,44 @@ export async function createPost(req: Request, res: Response) {
             img
         })
         await newPost.save()
-        res.status(200).json('Post create successfully')
+        res.status(200).json({message: 'Post created successfully'})
         
     } catch (error) {
         console.log(error)
         res.status(500).json({error: 'Internal server error'})
     }
+}
+
+export async function deletePost(req: Request, res: Response){
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            return res.status(404).json({error: 'Post not found'})
+        }
+        if (!req.user){
+            return res.status(404).json({error: 'User not found'})
+        }
+        if (post.user.toString() !== req.user._id.toString()){
+            return res.status(404).json({error: "You are not authorized to delete this post"})
+        }
+
+        if (post.img){
+            const publicId = getCloudinaryPublicId(post.img, 'post_images')
+            await claudinary.uploader.destroy(publicId)
+        }
+
+        const { id } = req.params
+        await Post.findByIdAndDelete(id)
+
+        res.status(200).json({message: "Post deleted successfully"})
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error: 'Internal server error'})
+    }
+}
+
+function getCloudinaryPublicId(url: string, folder: string): string {
+    const filename = url.split("/").pop()!.split(".")[0]
+    return `${folder}/${filename}`
 }
--- backend/routes/post.route.ts
+++ backend/routes/post.route.ts
@@ -1,8 +1,9 @@
 import express from "express";
 
 import { protectRoute } from "../middleware/protectRoute";
-import { createPost } from "../controllers/post.controller"
+import { createPost, deletePost } from "../controllers/post.controller"
 
 export const postRouter = express.Router()
 
 postRouter.post('/create', protectRoute, createPost)
+postRouter.delete('/:id', protectRoute, deletePost)
