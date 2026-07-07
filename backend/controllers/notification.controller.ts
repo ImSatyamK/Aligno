31a4509f17a6c832dfcd3d4f17c6d13e9aeeeb57
Satyam Kumar <189542961+ImSatyamK@users.noreply.github.com>
2026-07-07 18:23:22 +0530

    Feature: Delete Notifications

--- backend/controllers/notification.controller.ts
+++ backend/controllers/notification.controller.ts
@@ -28,3 +28,20 @@ export async function getAllNotifications(req: Request, res: Response){
         res.status(500).json({error: 'Internal server error'})
     }
 }
+
+export async function deleteNotifications(req: Request, res: Response){
+    try {
+        const userId = req.user!._id
+        const user = await User.findById(userId)
+        if (!user) {
+            return res.status(404).json({error: 'User not found'})
+        }
+
+        await Notification.deleteMany({ to: userId })
+
+        res.status(200).json({ message: 'Notifications deleted successfully' })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error: 'Internal server error'})
+    }
+}
--- backend/routes/notification.route.ts
+++ backend/routes/notification.route.ts
@@ -1,8 +1,9 @@
 import express from "express";
 
 import { protectRoute } from "../middleware/protectRoute";
-import { getAllNotifications } from "../controllers/notification.controller"
+import { getAllNotifications, deleteNotifications } from "../controllers/notification.controller"
 
 export const notificationRouter = express.Router()
 
 notificationRouter.get('/all', protectRoute, getAllNotifications)
+notificationRouter.delete('/', protectRoute, deleteNotifications)
