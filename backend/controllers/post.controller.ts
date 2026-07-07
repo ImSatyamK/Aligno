3f4f49316dea0399b60eabea54f33e4e375de552
Satyam Kumar <189542961+ImSatyamK@users.noreply.github.com>
2026-07-07 17:19:53 +0530

    Feature: Get all posts

--- backend/controllers/post.controller.ts
+++ backend/controllers/post.controller.ts
@@ -145,3 +145,30 @@ export async function commentOnPost(req: Request, res: Response){
         res.status(500).json({error: 'Internal server error'})
     }
 }
+
+export async function getAllPosts(req: Request, res: Response){
+    try {
+        const userId = req.user!._id
+        const user = await User.findById(userId)
+        if (!user) {
+            return res.status(404).json({error: 'User not found'})
+        }
+
+        const posts = await Post.find().sort({ createdAt: -1 }).populate({
+            path: 'user',
+            select: 'username profileImg'
+        }).populate({
+            path: 'comments.user',
+            select: 'username profileImg'
+        })
+
+        if (!posts) {
+            return res.status(200).json([])
+        }
+
+        res.status(200).json({ posts })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error: 'Internal server error'})
+    }
+}
--- backend/routes/post.route.ts
+++ backend/routes/post.route.ts
@@ -1,7 +1,7 @@
 import express from "express";
 
 import { protectRoute } from "../middleware/protectRoute";
-import { createPost, deletePost, likeUnlikePost, commentOnPost } from "../controllers/post.controller"
+import { createPost, deletePost, likeUnlikePost, commentOnPost, getAllPosts } from "../controllers/post.controller"
 
 export const postRouter = express.Router()
 
@@ -9,3 +9,5 @@ postRouter.post('/create', protectRoute, createPost)
 postRouter.delete('/:id', protectRoute, deletePost)
 postRouter.post('/like/:postId', protectRoute, likeUnlikePost)
 postRouter.post('/comment/:postId', protectRoute, commentOnPost)
+
+postRouter.get('/all', protectRoute, getAllPosts)
