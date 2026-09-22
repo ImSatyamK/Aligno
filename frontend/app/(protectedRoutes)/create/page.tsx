d2e1f026f47f3059b2d47ab027aafa3aefdec6e5
Satyam Kumar <189542961+ImSatyamK@users.noreply.github.com>
2026-09-22 14:28:03 +0530

    Fix: Deployement bug

--- frontend/app/(protectedRoutes)/create/page.tsx
+++ frontend/app/(protectedRoutes)/create/page.tsx
@@ -1,10 +1,12 @@
+export const dynamic = "force-dynamic";
+
 import { CreatePostForm } from '@/components/createPost.component'
 import { getCurrentUser } from '@/api/auth'
 
 export default async function CreatePostPage() {
     const currentUser = await getCurrentUser()
-    const currentUsername = currentUser?.data.username || null
-    const currentUserImg = currentUser?.data.profileImg || null
+    const currentUsername = currentUser?.data?.username || null
+    const currentUserImg = currentUser?.data?.profileImg || null
 
     return (
         <div className="max-w-xl mx-auto p-4">
